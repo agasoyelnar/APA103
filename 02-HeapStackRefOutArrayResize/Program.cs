20005bfa98ff1a5c8f0d95b69f3529d71a9068c1
Peyman <peyman@Peymans-MacBook-Air.local>
2026-03-15 17:56:20 +0400

    new

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3 };
+
+        int[] result = CustomArrResize(arr, 4, 5, 6);
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine(result[i]);
+        }
+    }
+
+    static int[] CustomArrResize(int[] arr, params int[] nums)
+    {
+        int[] result = new int[arr.Length + nums.Length];
+
+        int index = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            result[index] = arr[i];
+            index++;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[index] = nums[i];
+            index++;
+        }
+
+        return result;
+    }
+}
