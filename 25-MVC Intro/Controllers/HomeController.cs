a2fc4ee8ef027fecd298d00ad237f35b86b6c86d
Peyman <peyman@Peymans-MacBook-Air.local>
2026-05-05 00:25:32 +0400

    mvc1

---
+++ 25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if(id is null || id < 0){
+                return RedirectToAction(nameof(Error));
+            }
+            return RedirectToAction(nameof(Index),"Product");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
